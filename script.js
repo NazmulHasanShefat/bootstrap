0b05e9b2afb489858f365e943e621cbcc182d01b
NazmulHasanShefat <119728546+NazmulHasanShefat@users.noreply.github.com>
2024-07-16 15:45:58 +0600

    last commit

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let dumi_input = document.querySelector(".dumi_input");
 let search_box_back_shadow = document.querySelector(".search_box_back_shadow");
 let search_input_box = document.querySelector(".search_input_box");
 let suggestion_container = document.querySelector(".suggestion_container");
+let mobile_search = document.querySelector("#mobile_search");
 let element = document.body;
 
 th_1.addEventListener("click",()=>{
@@ -17,11 +18,16 @@ th_3.addEventListener("click",()=>{
 
 dumi_input.addEventListener("click",()=>{
    search_box_back_shadow.classList.toggle("active_search");
-   search_input_box.classList.toggle("active_search");
+   search_input_box.classList.toggle("active_search_m");
    suggestion_container.classList.toggle("active_search");
 })
 search_box_back_shadow.addEventListener("click",()=>{
    search_box_back_shadow.classList.toggle("active_search");
-   search_input_box.classList.toggle("active_search");
+   search_input_box.classList.toggle("active_search_m");
+   suggestion_container.classList.toggle("active_search");
+})
+mobile_search.addEventListener("click",()=>{
+   search_box_back_shadow.classList.toggle("active_search");
+   search_input_box.classList.toggle("active_search_m");
    suggestion_container.classList.toggle("active_search");
 })
